6e21e9149d73fd747dfe3758ae993c271792cb73
unknown <bba95033@NG00221352.cov.virginia.gov>
2014-08-21 11:09:45 -0400

    Added empty Overview map to map.js

--- map.js
+++ map.js
@@ -1,8 +1,18 @@
 
-require(["esri/map"], function(Map) {
+require(["esri/map", "esri/dijit/OverviewMap"], function(Map, OverviewMap) {
 	window.map = new Map("map", {
 		basemap: "osm",
 		center: [-75.97086160156249, 37.89287113281247],
 		zoom: 9
 	});
+	var overviewMapDijit = new OverviewMap({
+	    map: window.map,
+	    attachTo: "bottom-right",
+	    color:" #D84E13",
+	    opacity: .40,
+	    width: 250,
+	    height:250
+	});
+	overviewMapDijit.startup();
+	overviewMapDijit.show();
 });
