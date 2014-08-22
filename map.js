f104c5814ba9c4232b343710fa9fa38665c0a478
Tony Gambone <tony.gambone@deq.virginia.gov>
2014-08-22 13:20:06 -0400

    Media query tweak;

--- map.js
+++ map.js
@@ -60,7 +60,7 @@ require([
 
 	overviewMapDijit.startup();
 	if ('matchMedia' in window) {
-		var mq = window.matchMedia('(max-width: 600px)');
+		var mq = window.matchMedia('(max-width: 600px) and (orientation: portrait)');
 		if (mq.matches) overviewMapDijit.hide(); else overviewMapDijit.show();
 		mq.addListener(function(evt) {
 			if (mq.matches) overviewMapDijit.hide(); else overviewMapDijit.show();
