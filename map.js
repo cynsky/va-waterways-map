334e467aee0f403be87495eed1f9fd47f33dffd5
John N. Tragesser III <john.tragesser@deq.virginia.gov>
2014-08-26 14:17:44 -0400

    Revert "Testing whether we can hid the inset map by trying to detect max-height of 800 in landscape.  I have to commit to test it out on my phone.  I can easily revert if this test fails."
    This reverts commit 2d83f93015e1714aa58ed05eb08664ef024b7d2f.

--- map.js
+++ map.js
@@ -65,8 +65,7 @@ require([
 
 	overviewMapDijit.startup();
 	if ('matchMedia' in window) {
-		// var mq = window.matchMedia('(max-width: 600px) and (orientation: portrait)');
-		var mq = window.matchMedia('(max-width: 600px) and (orientation: portrait) || (max-height: 800px) and (orientation: landscape)');
+		var mq = window.matchMedia('(max-width: 600px) and (orientation: portrait)');
 		if (mq.matches) overviewMapDijit.hide(); else overviewMapDijit.show();
 		mq.addListener(function(evt) {
 			if (mq.matches) overviewMapDijit.hide(); else overviewMapDijit.show();
